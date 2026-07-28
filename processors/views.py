47b323400d243aba091348c5ed750c4237091edb
Urfas <urfasurfas@gmail.com>
2026-07-28 21:13:36 +0500

    Update views.py

--- processors/views.py
+++ processors/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Processor
 
 def comparison(request):
-    # Пока просто отдаём пустой список.
-    # Поиск и добавление сделаем на следующем шаге.
-    return render(request, 'processors/comparison.html', {
-        'processors': []
-    })
+    return render(request, 'processors/comparison.html')
+
+def search_processors(request):
+    query = request.GET.get('q', '').strip()
+    
+    if len(query) < 2:
+        return JsonResponse({'results': []})
+    
+    processors = Processor.objects.filter(name__icontains=query)[:15]
+    
+    results = []
+    for cpu in processors:
+        results.append({
+            'id': cpu.id,
+            'name': cpu.name,
+            'manufacturer': cpu.manufacturer,
+            'cores': cpu.cores,
+            'threads': cpu.threads,
+            'release_year': cpu.release_year,
+        })
+    
+    return JsonResponse({'results': results})
