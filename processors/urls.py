89655082f6a7ca947942a542929280c8d9465451
Urfas <urfasurfas@gmail.com>
2026-09-05 19:13:36 +0500

    Update urls.py

--- processors/urls.py
+++ processors/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.comparison, name='comparison'),
     path('api/search/', views.search_processors, name='search_processors'),
+    path('api/processor/<int:pk>/', views.processor_detail, name='processor_detail'),
 ]
